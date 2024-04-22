a2183f7338253eb9c4a438997fed9472f307a820
jjeong-HYU <99715505+jjeong-HYU@users.noreply.github.com>
2024-04-22 13:27:37 +0900

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -17,24 +17,23 @@ SetupEnv(){
     export G4_PATH_TOP=${PWD}
     touch ${G4_PATH_TOP}/setG4Env.sh
     cat /dev/null > ${G4_PATH_TOP}/setG4Env.sh
-    echo 'export G4_PATH_TOP=${PWD}' >> ${G4_PATH_TOP}/setG4Env.sh
-    echo 'export LD_LIBRARY_PATH=${G4_PATH_TOP}/lib-clhep:${G4_PATH_TOP}/lib_G4:${LD_LIBRARY_PATH}' >> ${G4_PATH_TOP}/setG4Env.sh
-    echo 'export G4LEVELGAMMADATA=${G4_PATH_TOP}/data-g4/PhotonEvaporation5.7' >> ${G4_PATH_TOP}/setG4Env.sh
-    echo 'export G4LEDATA=${G4_PATH_TOP}/data-g4/G4EMLOW8.2' >> ${G4_PATH_TOP}/setG4Env.sh
-    echo 'export G4NEUTRONHPDATA=${G4_PATH_TOP}/data-g4/G4NDL4.7' >> ${G4_PATH_TOP}/setG4Env.sh
-    echo 'export G4ENSDFSTATEDATA=${G4_PATH_TOP}/data-g4/G4ENSDFSTATE2.3' >> ${G4_PATH_TOP}/setG4Env.sh
-    echo 'export G4RADIOACTIVEDATA=${G4_PATH_TOP}/data-g4/RadioactiveDecay5.6' >> ${G4_PATH_TOP}/setG4Env.sh
-    echo 'export G4ABLADATA=${G4_PATH_TOP}/data-g4/G4ABLA3.1' >> ${G4_PATH_TOP}/setG4Env.sh
-    echo 'export G4PIIDATA=${G4_PATH_TOP}/data-g4/G4PII1.3' >> ${G4_PATH_TOP}/setG4Env.sh
-    echo 'export G4PARTICLEXSDATA=${G4_PATH_TOP}/data-g4/G4PARTICLEXS4.0' >> ${G4_PATH_TOP}/setG4Env.sh
-    echo 'export G4SAIDXSDATA=${G4_PATH_TOP}/data-g4/G4SAIDDATA2.0' >> ${G4_PATH_TOP}/setG4Env.sh
-    echo 'export G4REALSURFACEDATA=${G4_PATH_TOP}/data-g4/RealSurface2.2' >> ${G4_PATH_TOP}/setG4Env.sh
-    echo 'export G4INCLDATA=${G4_PATH_TOP}/data-g4/G4INCL1.0' >> ${G4_PATH_TOP}/setG4Env.sh
+    echo 'export LD_LIBRARY_PATH='${G4_PATH_TOP}'/lib-clhep:'${G4_PATH_TOP}'/lib_G4:${LD_LIBRARY_PATH}' >> ${G4_PATH_TOP}/setG4Env.sh
+    echo 'export G4LEVELGAMMADATA='${G4_PATH_TOP}'/data-g4/PhotonEvaporation5.7' >> ${G4_PATH_TOP}/setG4Env.sh
+    echo 'export G4LEDATA='${G4_PATH_TOP}'/data-g4/G4EMLOW8.2' >> ${G4_PATH_TOP}/setG4Env.sh
+    echo 'export G4NEUTRONHPDATA='${G4_PATH_TOP}'/data-g4/G4NDL4.7' >> ${G4_PATH_TOP}/setG4Env.sh
+    echo 'export G4ENSDFSTATEDATA='${G4_PATH_TOP}'/data-g4/G4ENSDFSTATE2.3' >> ${G4_PATH_TOP}/setG4Env.sh
+    echo 'export G4RADIOACTIVEDATA='${G4_PATH_TOP}'/data-g4/RadioactiveDecay5.6' >> ${G4_PATH_TOP}/setG4Env.sh
+    echo 'export G4ABLADATA='${G4_PATH_TOP}'/data-g4/G4ABLA3.1' >> ${G4_PATH_TOP}/setG4Env.sh
+    echo 'export G4PIIDATA='${G4_PATH_TOP}'/data-g4/G4PII1.3' >> ${G4_PATH_TOP}/setG4Env.sh
+    echo 'export G4PARTICLEXSDATA='${G4_PATH_TOP}'/data-g4/G4PARTICLEXS4.0' >> ${G4_PATH_TOP}/setG4Env.sh
+    echo 'export G4SAIDXSDATA='${G4_PATH_TOP}'/data-g4/G4SAIDDATA2.0' >> ${G4_PATH_TOP}/setG4Env.sh
+    echo 'export G4REALSURFACEDATA='${G4_PATH_TOP}'/data-g4/RealSurface2.2' >> ${G4_PATH_TOP}/setG4Env.sh
+    echo 'export G4INCLDATA='${G4_PATH_TOP}'/data-g4/G4INCL1.0' >> ${G4_PATH_TOP}/setG4Env.sh
     echo "Adding setG4Env.sh to ./bashrc? (1:yes / 2:no): "
     read option_bashrc
     if [ $option_bashrc -eq 1 ]; then
         export
-        echo 'source ${G4_PATH_TOP}/setG4Env.sh' >> ~/.bashrc
+        echo 'source '${G4_PATH_TOP}'/setG4Env.sh' >> ~/.bashrc
         source ~/.bashrc
     fi
 }
